c92de55e19e90772158073607b830e1de033d829
Sudo-Self <sudo-self@proton.me>
2024-08-28 17:21:56 -0600

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 useSeoMeta({
-  title: 'Draw and share your Art with Atidraw',
-  description: 'Atidraw is a web application that lets you to create, enhance, and share your drawings with the world. Harnessing the power of Cloudflare R2 and Cloudflare AI to store and enhance your drawings.',
-  ogTitle: 'Atidraw',
-  ogImage: 'https://assets.hub.nuxt.com/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJodHRwczovL2RyYXcubnV4dC5kZXYiLCJpYXQiOjE3MTgwMTc3OTF9.ORrHGOCvTaxN7Lk24swRO7k-mrHmExypBquOA8FUlyg.jpg?theme=light',
+  title: 'share your doodles',
+  description: 'doodle nuxt is a web application that lets you to create, enhance, and share your drawings with the world. Harnessing the power of Cloudflare R2 and Cloudflare AI to store and enhance your drawings.',
+  ogTitle: 'doodle',
+  ogImage: 'https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/icon512.png?theme=light',
   twitterCard: 'summary_large_image',
-})
 useServerHead({
   meta: [
-    { name: 'google-site-verification', content: 'ocIv8ZYQ5s35CIAYVpjPYyhBnWu7mo0JpdzOeWV4PZs' },
+    { name: 'description', content: 'doodle nuxt dev' },
+    { name: 'keywords', content: 'doodle, nuxt, dev, share' },
   ],
 })
+
 </script>
 
 <template>
