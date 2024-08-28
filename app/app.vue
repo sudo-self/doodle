3cd0b4e4783c36e9451dfc2a127f131aeb37cc01
Sudo-Self <sudo-self@proton.me>
2024-08-28 17:26:45 -0600

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
+import { useSeoMeta, useServerHead } from 'nuxt';
+
+//SEO
 useSeoMeta({
   title: 'share your doodles',
-  description: 'doodle nuxt is a web application that lets you to create, enhance, and share your drawings with the world. Harnessing the power of Cloudflare R2 and Cloudflare AI to store and enhance your drawings.',
+  description: 'doodle nuxt is a web application that lets you create, enhance, and share your drawings with the world. Harnessing the power of Cloudflare R2 and Cloudflare AI to store and enhance your drawings.',
   ogTitle: 'doodle',
   ogImage: 'https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/icon512.png?theme=light',
   twitterCard: 'summary_large_image',
+})
+
+//meta tags
 useServerHead({
   meta: [
     { name: 'description', content: 'doodle nuxt dev' },
     { name: 'keywords', content: 'doodle, nuxt, dev, share' },
   ],
 })
-
 </script>
+
 
 <template>
   <AppHeader />
