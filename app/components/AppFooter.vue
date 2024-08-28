3e97e7d3b30d5f686520b6608c4bdfead4ed181d
Sudo-Self <sudo-self@proton.me>
2024-08-28 16:06:38 -0600

    Update AppFooter.vue

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -42,38 +42,35 @@ const isDark = computed({
           icon="i-simple-icons-x"
         />
       </div>
-      
- <div class="flex items-center justify-center lg:justify-start lg:flex-1 gap-x-1.5 mt-3 lg:mt-0 lg:order-1">
-  <span class="text-sm">
-    hosted by&nbsp;
-    <UButton
-      to="https://JesseJese.com"
-      variant="link"
-      class="text-orange-500" <!-- Apply the orange color -->
-      :padded="false"
-    >
-      cloudflare&nbsp;
-    </UButton>
-    <span class="text-green-500">JesseJesse</span>.com&nbsp; <!-- Apply the green color -->
-    <UButton
-      to="https://jessejesse.com"
-      variant="link"
-      class="text-black" <!-- If needed, apply black color via Tailwind class -->
-      :padded="false"
-    >
-      &nbsp;Nuxt &&nbsp;
-    </UButton>
-    <UButton
-      to="https://hub.nuxt.com/?utm_source=atidraw"
-      variant="link"
-      class="text-black" <!-- If needed, apply black color via Tailwind class -->
-      :padded="false"
-    >
-      Nuxthub
-    </UButton>.
-  </span>
-</div>
-
+      <div class="flex items-center justify-center lg:justify-start lg:flex-1 gap-x-1.5 mt-3 lg:mt-0 lg:order-1">
+        <span class="text-sm">
+          hosted by&nbsp;
+          <UButton
+            to="https://JesseJese.com"
+            variant="link"
+            :padded="false"
+            class="text-orange-500" <!-- Use class to style color -->
+          >
+            cloudflare&nbsp;
+          </UButton>
+          <span class="text-green-500">JesseJesse</span>.com&nbsp;
+          <UButton
+            to="https://jessejesse.com"
+            variant="link"
+            :padded="false"
+          >
+            &nbsp;Nuxt &&nbsp;
+          </UButton>
+          <UButton
+            to="https://hub.nuxt.com/?utm_source=atidraw"
+            variant="link"
+            :padded="false"
+          >
+            Nuxthub
+          </UButton>
+        </span>
+      </div>
     </UContainer>
   </footer>
 </template>
+
