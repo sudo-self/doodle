a82d3276f123d410185f3be386879a8ad2c4145e
Sudo-Self <sudo-self@proton.me>
2024-08-28 20:03:21 -0600

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -6,7 +6,7 @@ useSeoMeta({
   title: 'Doodles',
   description: 'Doodle Nuxt is a web application that lets you create, enhance, and share your drawings with the world. Harnessing the power of Cloudflare R2 and Cloudflare AI to store and enhance your drawings.',
   ogTitle: 'Doodle',
-  ogImage: 'https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/icon512.png?theme=light',
+  ogImage: 'public/362505213-27d832a8-92e1-43a9-a96b-c0990c0a617d.png',
   twitterCard: 'summary_large_image'
 });
 </script>
