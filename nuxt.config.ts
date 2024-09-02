b1933781e92255648555c1497e1c16947ca2c741
Sudo-Self <sudo-self@proton.me>
2024-09-02 14:09:31 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-30',
   future: { compatibilityVersion: 4 },
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
       googleAuth: Boolean(process.env.GOOGLE_AUTH || (process.env.NUXT_OAUTH_GOOGLE_CLIENT_ID && process.env.NUXT_OAUTH_GOOGLE_CLIENT_SECRET)),
     },
   },
-  // Development modules
+
   eslint: {
     config: {
       stylistic: {
